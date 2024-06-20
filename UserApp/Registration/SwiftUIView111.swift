081a32ba80beafe6a91e4cb7981df8e895ab6591
YARBASH K <121942818+yarbashsysitco@users.noreply.github.com>
2024-06-20 16:41:05 +0530

    test12

---
+++ UserApp/Registration/SwiftUIView111.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView111.swift
+//  UserApp
+//
+//  Created by admin on 20/06/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView111: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView111()
+}
